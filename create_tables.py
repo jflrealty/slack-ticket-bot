3787abeb52c4f4fb467ceea7971e566fec273c55
jflrealty <83607505+jflrealty@users.noreply.github.com>
2025-04-01 16:05:50 -0300

    Delete create_tables.py

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-from models import Base
-from database import engine
-
-if __name__ == "__main__":
-    print("🔧 Criando tabelas no banco...")
-    Base.metadata.create_all(bind=engine)
-    print("✅ Tabelas criadas com sucesso!")
